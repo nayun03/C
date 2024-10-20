687c07413fc48f0e079ec0583c4da8afbea17742
nayun03 <nayun.jeong001@umb.edu>
2024-10-20 16:08:59 -0400

    Week1: Print Basic Shape

---
+++ PrintBasicShape.cpp
@@ -0,0 +1,62 @@
+/*********************************************************************
+* Name: Nayun Jeong
+* Project 1: PrintBasicShapes
+* Editor(s) used: Visual Studio Code
+* Compiler(s) used: Visual Studio Code
+* Description:
+*   This program print triangle, rectangle, and diamond.
+**********************************************************************/
+#include <iostream>
+using namespace std;
+
+/*********************************************************************
+* Function: Main
+**********************************************************************/
+
+int main() {
+
+    cout << "Triangle: ";
+    cout << endl;
+    cout << "       *      ";
+    cout << endl;
+    cout << "      ***     ";
+    cout << endl;
+    cout << "     *****    ";
+    cout << endl;
+    cout << "    *******   ";
+    cout << endl;
+    cout << "   *********  ";
+    cout << endl;
+    cout << endl;
+
+    cout << "Rectangle: ";
+    cout << endl;
+    cout << "   ##########   ";
+    cout << endl;
+    cout << "   #        #   ";
+    cout << endl;
+    cout << "   #        #   ";
+    cout << endl;
+    cout << "   #        #   ";
+    cout << endl;
+    cout << "   ##########   ";
+    cout << endl;
+
+    cout << endl;
+    cout << "Diamond: ";
+    cout << endl;
+    cout << "     %     " << endl;
+    cout << "   %   %   " << endl;
+    cout << "  %     %  " << endl;
+    cout << "   %   %   " << endl;
+    cout << "     %     " << endl;
+
+    return 0;
+}
+
+/*********************************************************************
+* Function: Prints a triangle, rectangle, and diamond using different 
+* characters for each shape, all with exactly 5 lines as requested
+*
+* Return: NONE
+**********************************************************************/
